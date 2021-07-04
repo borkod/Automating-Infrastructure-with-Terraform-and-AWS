fe3a5f269cf5ae0921c8a99a0841a0de093a07f9
Borko <borkod@gmail.com>
2021-07-03 21:13:59 -0400

    Created ECR and Website Docker image.

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import React, { useState } from 'react'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <div className="container">
+        <h2>The Future Home of Koffee Luv</h2>
+      </div>
+    </div>
+  )
+}
+
+export default App
